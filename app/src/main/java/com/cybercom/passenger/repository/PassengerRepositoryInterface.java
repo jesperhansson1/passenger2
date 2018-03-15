db015dffb83ba71bc44201af2035b96a5d185b05
Jesper Hansson <jesper.hansson@cybercom.com>
2018-03-15 13:47:00 +0100

    Add repository interface
    Add first draft of the repository interface

---
+++ app/src/main/java/com/cybercom/passenger/repository/PassengerRepositoryInterface.java
@@ -0,0 +1,23 @@
+package com.cybercom.passenger.repository;
+
+import android.arch.lifecycle.LiveData;
+
+import com.cybercom.passenger.model.Drive;
+import com.cybercom.passenger.model.DriveRequest;
+import com.cybercom.passenger.model.User;
+
+import java.util.List;
+
+public interface PassengerRepositoryInterface {
+    LiveData<User> getUser();
+
+    void updateUserType(int type);
+
+    void createUser(User user);
+
+    LiveData<List<Drive>> getDrives();
+
+    void addDrive(Drive drive);
+
+    void addDriveRequest(DriveRequest DriveRequest);
+}
