097cae2d4ef0ca2ef7df8b546ed271fdebe3e738
Andreas Vettefors <andreas.vettefors@cybercom.com>
2018-03-16 14:13:18 +0100

    Implement  addDriveRequest method
    Add drive request to firebase
    TODO: Deside how we identify each drive request

--- app/src/main/java/com/cybercom/passenger/repository/PassengerRepository.java
+++ app/src/main/java/com/cybercom/passenger/repository/PassengerRepository.java
@@ -11,21 +11,28 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class PassengerRepository implements PassengerRepositoryInterface {
 
     private static final String REFERENCE_USERS = "users";
-    private static final String USER_CHILD_TYPE = "type";
+    private static final String REFERENCE_DRIVES = "drives";
+    private static final String REFERENCE_DRIVE_REQUESTS = "driveRequests";
+    private static final String REFERENCE_USERS_CHILD_TYPE = "type";
     private static final String MOCK_USER = "userone";
     private static final String MOCK_DRIVE = "driveone";
-    private static final String REFERENCE_DRIVES = "drives";
+    private static final String MOCK_DRIVEREQUEST = "driverequestone";
+
 
     private static PassengerRepository sPassengerRepository;
     private DatabaseReference mUsersReference;
     private DatabaseReference mDrivesReference;
+    private DatabaseReference mDriveRequestsReference;
 
-    public static PassengerRepository getInstance(){
+
+    public static PassengerRepository getInstance() {
         if (sPassengerRepository == null) {
             sPassengerRepository = new PassengerRepository();
         }
@@ -36,6 +43,7 @@ public class PassengerRepository implements PassengerRepositoryInterface {
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         mUsersReference = firebaseDatabase.getReference(REFERENCE_USERS);
         mDrivesReference = firebaseDatabase.getReference(REFERENCE_DRIVES);
+        mDriveRequestsReference = firebaseDatabase.getReference(REFERENCE_DRIVE_REQUESTS);
     }
 
     @Override
@@ -43,24 +51,24 @@ public class PassengerRepository implements PassengerRepositoryInterface {
 
         final MutableLiveData<User> user = new MutableLiveData<>();
 
-         mUsersReference.child(MOCK_USER).addValueEventListener(new ValueEventListener() {
-             @Override
-             public void onDataChange(DataSnapshot dataSnapshot) {
-                 user.setValue(dataSnapshot.getValue(User.class));
-             }
+        mUsersReference.child(MOCK_USER).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                user.setValue(dataSnapshot.getValue(User.class));
+            }
 
-             @Override
-             public void onCancelled(DatabaseError databaseError) {
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-             }
+            }
 
-         });
+        });
         return user;
     }
 
     @Override
     public void updateUserType(int type) {
-        mUsersReference.child(MOCK_USER).child(USER_CHILD_TYPE).setValue(type);
+        mUsersReference.child(MOCK_USER).child(REFERENCE_USERS_CHILD_TYPE).setValue(type);
     }
 
     @Override
@@ -70,8 +78,6 @@ public class PassengerRepository implements PassengerRepositoryInterface {
 
     @Override
     public LiveData<List<Drive>> getDrives() {
-
-
         return null;
     }
 
@@ -82,6 +88,6 @@ public class PassengerRepository implements PassengerRepositoryInterface {
 
     @Override
     public void addDriveRequest(DriveRequest driveRequest) {
-
+        mDriveRequestsReference.child(MOCK_DRIVEREQUEST).setValue(driveRequest);
     }
 }
