b43d018560480540a1e6294e551200f83c6c0096
CYBERDOM2\rakap1 <Rajani.Kaparthy@cybercom.com>
2018-03-19 15:03:34 +0100

    Add UI design and color for create drive dialog
    Add UI design, dimension, position and color for request ride dialog
    Add UI design, dimension, position and color for passenger driver switch

--- app/src/main/java/com/cybercom/passenger/flows/main/MainActivity.java
+++ app/src/main/java/com/cybercom/passenger/flows/main/MainActivity.java
@@ -1,13 +1,16 @@
 package com.cybercom.passenger.flows.main;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
 import com.cybercom.passenger.R;
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addUI(){
+        changeLabelFontStyle(false);
         final Switch switchRide = findViewById(R.id.switch_ride);
         final FloatingActionButton floatRide = findViewById(R.id.button_createRide);
         floatRide.setImageResource(R.drawable.passenger);
@@ -37,17 +41,19 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton arg0, boolean isChecked) {
                 if(isChecked){
                     floatRide.setImageResource(R.drawable.driver);
+                    changeLabelFontStyle(true);
                 }
                 else
                 {
                     floatRide.setImageResource(R.drawable.passenger);
+                    changeLabelFontStyle(false);
                 }
             }
         });
         floatRide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 if(switchRide.isChecked()){
+                if(switchRide.isChecked()){
                     showCreateDriveDialog();
                 }
                 else
@@ -58,17 +64,32 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void changeLabelFontStyle(boolean driverValue)
+    {
+        if(driverValue){
+            ((TextView)findViewById(R.id.label_driver)).setTypeface(Typeface.DEFAULT_BOLD);
+            ((TextView)findViewById(R.id.label_passenger)).setTypeface(Typeface.DEFAULT);
+        }
+        else
+        {
+            ((TextView)findViewById(R.id.label_passenger)).setTypeface(Typeface.DEFAULT_BOLD);
+            ((TextView)findViewById(R.id.label_driver)).setTypeface(Typeface.DEFAULT);
+        }
+    }
+
     public void showCreateDriveDialog()
     {
         CreateRideDialog createRideDialog = new CreateRideDialog(MainActivity.this, "Create Drive");
-        createRideDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        createRideDialog.getWindow().setGravity(Gravity.BOTTOM);
+        createRideDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         createRideDialog.show();
     }
 
     public void showCreateDriveRequestDialog()
     {
         CreateRideDialog createRideDialog = new CreateRideDialog(MainActivity.this, "Request");
-        createRideDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        createRideDialog.getWindow().setGravity(Gravity.BOTTOM);
+        createRideDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         createRideDialog.show();
     }
 }
