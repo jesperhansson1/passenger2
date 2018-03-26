2da1ad26f9b1011e423e3b5bcb6c752edd3284ed
CYBERDOM2\johaa1 <jonas.haag@cybercom.com>
2018-03-26 17:03:47 +0200

    save matching

--- app/src/main/java/com/cybercom/passenger/repository/PassengerRepository.java
+++ app/src/main/java/com/cybercom/passenger/repository/PassengerRepository.java
@@ -2,9 +2,11 @@ package com.cybercom.passenger.repository;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.location.Location;
 
 import com.cybercom.passenger.model.Drive;
 import com.cybercom.passenger.model.DriveRequest;
+import com.cybercom.passenger.model.Position;
 import com.cybercom.passenger.model.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -12,9 +14,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.sql.Driver;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
+
+import timber.log.Timber;
 
 public class PassengerRepository implements PassengerRepositoryInterface {
 
@@ -95,6 +101,87 @@ public class PassengerRepository implements PassengerRepositoryInterface {
         return drivesList;
     }
 
+    public Drive findBestRideMatch(final DriveRequest driveRequest) {
+        final DriveRequest getDriveRequest = new DriveRequest(driveRequest.getTime(), driveRequest.getStartLocation(),
+                driveRequest.getEndLocation(), driveRequest.getNotificationTokenId(), driveRequest.getExtraPassengers());
+
+       final ArrayList getTime = new ArrayList<>();
+       final ArrayList<Position> getStartLocation = new ArrayList();
+       final ArrayList<Position> getEndLocation = new ArrayList();
+
+       final ArrayList filterTimeDrives = new ArrayList<>();
+       final ArrayList<Position> filterStartLocation = new ArrayList();
+       final ArrayList<Position> filtertEndLocation = new ArrayList();
+        final ArrayList sortDistanceBetween = new ArrayList<>();
+
+
+
+
+        mDrivesReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Drive bestMatch = null;
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Drive drive = snapshot.getValue(Drive.class);
+
+                    /*
+                    if (getDriveRequest != null && Math.abs( getDriveRequest.getTime() - Long.parseLong(getTime.get(i).toString())) < (60 * 60 * 1000)) {
+                        if (bestMatch == null) {
+                            bestMatch = drive;
+                            // räkna ut straäckn till startLocation ( DriveRequets.startLocation , drive.startLocation)
+                            // long shortestDistance = ...;
+                        }
+
+                        // Räkna ut sträckan mellan startpunkterna jämför med shortsDistance
+
+                        // om kortare sätt ny bestMatch = drive
+                        // samt shortestDistance = ny sträcka
+
+
+                        getTime.add(drive.getTime());
+                    }
+                    getStartLocation.add(drive.getStartLocation());
+                    */
+                }
+
+                    for (int i = 0; i < getTime.size(); i++) {
+
+                        if (getDriveRequest != null && Math.abs( getDriveRequest.getTime() - Long.parseLong(getTime.get(i).toString())) < (60 * 60 * 1000)) {
+                            filterTimeDrives.add(getTime.get(i));
+                            filterStartLocation.add(getStartLocation.get(i));
+                        }
+                    }
+
+                if(filterTimeDrives.size() == 1){
+                    Timber.d("HELLOOO one drive");
+                    //if there is only one drive, send to viewmodel
+                } else if (filterTimeDrives.size() == 0){
+                    Timber.d("HELLOOO No drive matched!");
+                } else {
+                    Timber.d("HELLOOO More than one drive");
+                    //if there are more than one, calculate the distance!
+                    float[] distance = new float[filterTimeDrives.size()];
+
+                    for(int i = 0; i < filterStartLocation.size(); i++){
+                        Location.distanceBetween(driveRequest.getStartLocation().getLatitude(), driveRequest.getStartLocation().getLongitude(),
+                                filterStartLocation.get(i).getLatitude(), filterStartLocation.get(i).getLongitude(), distance);
+                        sortDistanceBetween.add(distance[i]);
+                    }
+                    Collections.sort(sortDistanceBetween);
+                    System.out.println("dist " + sortDistanceBetween);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return null;
+    }
+
     @Override
     public void addDrive(Drive drive) {
         mDrivesReference.child(generateRandomUUID()).setValue(drive);
@@ -102,6 +189,7 @@ public class PassengerRepository implements PassengerRepositoryInterface {
 
     @Override
     public void addDriveRequest(DriveRequest driveRequest) {
+        findBestRideMatch(driveRequest);
         mDriveRequestsReference.child(generateRandomUUID()).setValue(driveRequest);
     }
 
