fc36c8e312c3e5cdde64adf63827c542de827ab5
CYBERDOM2\rakap1 <Rajani.Kaparthy@cybercom.com>
2018-03-29 13:22:20 +0200

    Update user model Add fullname, phone, email, password, imagelink, personal number, gender.

--- app/src/main/java/com/cybercom/passenger/model/User.java
+++ app/src/main/java/com/cybercom/passenger/model/User.java
@@ -2,6 +2,100 @@ package com.cybercom.passenger.model;
 
 public class User {
 
+    private String mPhone;
+    private String mPassword;
+    private String mEmail;
+    private String mPersonalNumber;
+    private String mFullName;
+    private String mImageLink;
+    private String mGender;
+    
+    public User(){
+
+    }
+
+    public User(String phone, String password, String email, String personalNumber, String fullName, String imageLink, String gender) {
+        this.mPhone = phone;
+        this.mPassword = password;
+        this.mEmail = email;
+        this.mPersonalNumber = personalNumber;
+        this.mFullName = fullName;
+        this.mImageLink = imageLink;
+        this.mGender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "mPhone='" + mPhone + '\'' +
+                ", mPassword='" + mPassword + '\'' +
+                ", mEmail='" + mEmail + '\'' +
+                ", mPersonalNumber='" + mPersonalNumber + '\'' +
+                ", mFullName='" + mFullName + '\'' +
+                ", mImageLink='" + mImageLink + '\'' +
+                ", mGender='" + mGender + '\'' +
+                '}';
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public void setPhone(String phone) {
+        this.mPhone = phone;
+    }
+
+    public String getPassword() {
+        return mPassword;
+    }
+
+    public void setPassword(String password) {
+        this.mPassword = password;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String email) {
+        this.mEmail = email;
+    }
+
+    public String getPersonalNumber() {
+        return mPersonalNumber;
+    }
+
+    public void setPersonalNumber(String personalNumber) {
+        this.mPersonalNumber = personalNumber;
+    }
+
+    public String getFullName() {
+        return mFullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.mFullName = fullName;
+    }
+
+    public String getImageLink() {
+        return mImageLink;
+    }
+
+    public void setImageLink(String imageLink) {
+        this.mImageLink = imageLink;
+    }
+
+    public String getGender() {
+        return mGender;
+    }
+
+    public void setGender(String gender) {
+        this.mGender = gender;
+    }
+
+
+    /*
+
     public static final int TYPE_DRIVER = 0;
     public static final int TYPE_PASSENGER = 1;
 
@@ -9,10 +103,6 @@ public class User {
     private String mLastName;
     private String mNotificationTokenId;
     private int mType;
-    
-    public User(){
-
-    }
 
     public User(String firstName, String lastName, String notificationTokenId, int type) {
         mFirstName = firstName;
@@ -61,5 +151,7 @@ public class User {
                 ", mNotificationTokenId='" + mNotificationTokenId + '\'' +
                 ", mType=" + mType +
                 '}';
-    }
+    }*/
+
+
 }
