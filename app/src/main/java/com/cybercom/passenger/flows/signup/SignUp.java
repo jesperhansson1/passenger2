bc489d4d4cde4e30aa7cfc8e1312f1d3f4d562a6
CYBERDOM2\johaa1 <jonas.haag@cybercom.com>
2018-03-29 15:26:01 +0200

    Create edittext model and toolbar

---
+++ app/src/main/java/com/cybercom/passenger/flows/signup/SignUp.java
@@ -0,0 +1,22 @@
+package com.cybercom.passenger.flows.signup;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import com.cybercom.passenger.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.signup_title);
+    }
+}
