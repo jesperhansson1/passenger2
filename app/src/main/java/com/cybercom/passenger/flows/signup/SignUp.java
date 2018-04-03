7dab16ed5afa9812e1ee90c2d12bee2305762723
CYBERDOM2\johaa1 <jonas.haag@cybercom.com>
2018-04-03 15:53:39 +0200

    Add Sign Up UI design
    Started to implement the signup UI

--- app/src/main/java/com/cybercom/passenger/flows/signup/SignUp.java
+++ app/src/main/java/com/cybercom/passenger/flows/signup/SignUp.java
@@ -1,15 +1,18 @@
 package com.cybercom.passenger.flows.signup;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.RadioButton;
 
 import com.cybercom.passenger.R;
 
-public class SignUp extends AppCompatActivity {
+import timber.log.Timber;
 
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+
+    RadioButton mRadioButtonMale, mRadioButtonFemale;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +21,54 @@ public class SignUp extends AppCompatActivity {
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.signup_title);
+
+        mRadioButtonMale = findViewById(R.id.maleRadioButton);
+        mRadioButtonMale.setOnClickListener(this);
+        mRadioButtonFemale = findViewById(R.id.femaleRadioButton);
+        mRadioButtonFemale.setOnClickListener(this);
+
+        mRadioButtonMale.setBackgroundColor(getResources().getColor(R.color.colorBlue));
+        mRadioButtonMale.setTextColor(getResources().getColor(R.color.colorWhite));
+        mRadioButtonFemale.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+        mRadioButtonFemale.setTextColor(getResources().getColor(R.color.colorBlue));
+
+        mRadioButtonMale.setButtonDrawable(R.drawable.ic_male_white);
+        mRadioButtonFemale.setButtonDrawable(R.drawable.ic_woman_blue);
+
+        if(mRadioButtonMale.isChecked()){
+            Timber.d("Radio Button MALE");
+        } else if(mRadioButtonFemale.isChecked()){
+            Timber.d("Radio Button FEMALE");
+        }
     }
+
+    @Override
+    public void onClick(View v) {
+        if(mRadioButtonMale.isChecked()){
+            Timber.d("Radio Button MALE");
+        } else if(mRadioButtonFemale.isChecked()){
+            Timber.d("Radio Button FEMALE");
+        }
+        switch(v.getId()) {
+            case R.id.maleRadioButton:
+                // Do something
+                mRadioButtonMale.setBackgroundColor(getResources().getColor(R.color.colorBlue));
+                mRadioButtonMale.setTextColor(getResources().getColor(R.color.colorWhite));
+                mRadioButtonMale.setButtonDrawable(R.drawable.ic_male_white);
+
+                mRadioButtonFemale.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+                mRadioButtonFemale.setTextColor(getResources().getColor(R.color.colorBlue));
+                mRadioButtonFemale.setButtonDrawable(R.drawable.ic_woman_blue);
+                break;
+            case R.id.femaleRadioButton:
+                // Do something
+                mRadioButtonFemale.setBackgroundColor(getResources().getColor(R.color.colorBlue));
+                mRadioButtonFemale.setTextColor(getResources().getColor(R.color.colorWhite));
+                mRadioButtonFemale.setButtonDrawable(R.drawable.ic_woman_white);
+
+                mRadioButtonMale.setBackgroundColor(getResources().getColor(R.color.colorWhite));
+                mRadioButtonMale.setTextColor(getResources().getColor(R.color.colorBlue));
+                mRadioButtonMale.setButtonDrawable(R.drawable.ic_male_blue);
+                break;
+        }    }
 }
