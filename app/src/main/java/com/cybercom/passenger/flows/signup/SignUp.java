6dd91c619c27bf485180a6482a8d270d4c849bf6
CYBERDOM2\johaa1 <jonas.haag@cybercom.com>
2018-04-04 13:43:01 +0200

    Implement firebase signup

--- app/src/main/java/com/cybercom/passenger/flows/signup/SignUp.java
+++ app/src/main/java/com/cybercom/passenger/flows/signup/SignUp.java
@@ -1,16 +1,34 @@
 package com.cybercom.passenger.flows.signup;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
+
 import com.cybercom.passenger.R;
+import com.cybercom.passenger.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
+    FirebaseAuth mAuth;
+
+    SignUpViewModel viewModel;
 
     RadioButton mRadioButtonMale, mRadioButtonFemale;
+    Button mNextButton;
+    EditText mPassword, mEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +38,20 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.signup_title);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        viewModel = ViewModelProviders.of(this).get(SignUpViewModel.class);
+
         mRadioButtonMale = findViewById(R.id.radiobutton_signup_maleRadioButton);
         mRadioButtonMale.setOnClickListener(this);
         mRadioButtonFemale = findViewById(R.id.radiobutton_signup_femaleRadioButton);
         mRadioButtonFemale.setOnClickListener(this);
+        mNextButton = findViewById(R.id.button_signup_next);
+        mNextButton.setOnClickListener(this);
+
+        mPassword = findViewById(R.id.edittext_signup_password);
+        mEmail = findViewById(R.id.edittext_signup_email);
+
 
         mRadioButtonMale.setBackgroundColor(getResources().getColor(R.color.colorBlue));
         mRadioButtonMale.setTextColor(getResources().getColor(R.color.colorWhite));
@@ -32,6 +60,8 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
         mRadioButtonMale.setButtonDrawable(R.drawable.ic_male_white);
         mRadioButtonFemale.setButtonDrawable(R.drawable.ic_woman_blue);
+
+
     }
 
     @Override
@@ -57,5 +87,34 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
                 mRadioButtonMale.setTextColor(getResources().getColor(R.color.colorBlue));
                 mRadioButtonMale.setButtonDrawable(R.drawable.ic_male_blue);
                 break;
-        }    }
+
+            case R.id.button_signup_next:
+                /*User user = new User("Hej", "wärlden", "s",0);
+                viewModel.createUser(user);*/
+                FirebaseUser user = mAuth.getInstance().getCurrentUser();
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("TAG", "createUserWithEmail:success");
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                                }
+                            }
+                        });
+
+
+                if(user != null){
+                    viewModel.createUser(new User(email, mAuth.getUid(), "s",0));
+                }
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/com/cybercom/passenger/flows/signup/SignUpViewModel.java
@@ -0,0 +1,23 @@
+package com.cybercom.passenger.flows.signup;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+import com.cybercom.passenger.model.User;
+import com.cybercom.passenger.repository.PassengerRepository;
+
+import timber.log.Timber;
+
+public class SignUpViewModel extends AndroidViewModel {
+    PassengerRepository repository = PassengerRepository.getInstance();
+
+    public SignUpViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void createUser(User user){
+        Timber.d("Next user from viewmodel %s", user);
+        repository.createUser(user);
+    }
+}
