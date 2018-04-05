c10a59eb79e71b64166af4e01c46d51e5c1eee2a
CYBERDOM2\johaa1 <jonas.haag@cybercom.com>
2018-04-05 16:46:17 +0200

    Add toolbar and login title string

--- app/src/main/java/com/cybercom/passenger/flows/login/Login.java
+++ app/src/main/java/com/cybercom/passenger/flows/login/Login.java
@@ -3,6 +3,7 @@ package com.cybercom.passenger.flows.login;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.cybercom.passenger.R;
 
@@ -16,5 +17,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.login_title);
     }
 }
