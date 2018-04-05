a04cf234d825ce9b681f107175519f75c9c0c126
CYBERDOM2\johaa1 <jonas.haag@cybercom.com>
2018-04-05 11:07:48 +0200

    Clean code before push

--- app/src/main/java/com/cybercom/passenger/flows/signup/SignUpViewModel.java
+++ app/src/main/java/com/cybercom/passenger/flows/signup/SignUpViewModel.java
@@ -3,12 +3,8 @@ package com.cybercom.passenger.flows.signup;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.support.annotation.NonNull;
-
 import com.cybercom.passenger.model.User;
 import com.cybercom.passenger.repository.PassengerRepository;
-import com.google.firebase.auth.FirebaseUser;
-
-import timber.log.Timber;
 
 public class SignUpViewModel extends AndroidViewModel {
     PassengerRepository repository = PassengerRepository.getInstance();
@@ -18,7 +14,6 @@ public class SignUpViewModel extends AndroidViewModel {
     }
 
     public void createUser(String userId, User user){
-        Timber.d("Next user from viewmodel %s", user);
         repository.createUser(userId, user);
     }
 }
