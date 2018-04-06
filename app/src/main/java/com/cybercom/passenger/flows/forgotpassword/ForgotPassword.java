da6eeb053782508d80bd856b53e153dd87fe0a36
CYBERDOM2\johaa1 <jonas.haag@cybercom.com>
2018-04-06 14:01:28 +0200

    Create ForgotPassword file

---
+++ app/src/main/java/com/cybercom/passenger/flows/forgotpassword/ForgotPassword.java
@@ -0,0 +1,15 @@
+package com.cybercom.passenger.flows.forgotpassword;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cybercom.passenger.R;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_password);
+    }
+}
