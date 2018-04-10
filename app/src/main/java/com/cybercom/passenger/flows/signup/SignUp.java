7041d477ad43ca60ac9ca4774c0706c25e96c8a2
CYBERDOM2\rakap1 <Rajani.Kaparthy@cybercom.com>
2018-04-10 14:11:00 +0200

    Pick avatar image from camera image picker intent photo library or gallery camera

--- app/src/main/java/com/cybercom/passenger/flows/signup/SignUp.java
+++ app/src/main/java/com/cybercom/passenger/flows/signup/SignUp.java
@@ -1,25 +1,47 @@
 package com.cybercom.passenger.flows.signup;
 
+import android.Manifest;
+import android.app.Activity;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.TextView;
+
 import com.cybercom.passenger.R;
 import com.cybercom.passenger.flows.main.MainActivity;
 import com.cybercom.passenger.model.User;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import timber.log.Timber;
 
-public class SignUp extends AppCompatActivity implements View.OnClickListener {
+public class SignUp extends AppCompatActivity implements View.OnClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
     SignUpViewModel mViewModel;
     RadioButton mRadioButtonMale, mRadioButtonFemale;
@@ -28,6 +50,8 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     String mSaveRadioButtonAnswer;
     private static final String GENDER_MALE = "Male";
     private static final String GENDER_FEMALE = "Female";
+    ImageView mImageViewProfile;
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +86,8 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         mRadioButtonFemale.setButtonDrawable(R.drawable.ic_woman_blue);
 
         mSaveRadioButtonAnswer = GENDER_MALE;
+        mImageViewProfile = findViewById(R.id.imageview_signup_profile);
+        mImageViewProfile.setOnClickListener(this);
     }
 
     @Override
@@ -112,6 +138,133 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
                     }
                 });
                 break;
+
+            case R.id.imageview_signup_profile:
+                checkpermissions(SignUp.this);
+                break;
         }
+    }
+
+    public void checkpermissions(Activity activity) {
+        PackageManager mPackageManager = activity.getPackageManager();
+        int hasPermStorage = mPackageManager.checkPermission(android.Manifest.permission.CAMERA, activity.getPackageName());
+        if (hasPermStorage != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CAMERA_PERMISSION);
+        } else if (hasPermStorage == PackageManager.PERMISSION_GRANTED) {
+            openMediaSelector(SignUp.this);
+        } else {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CAMERA_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION){
+            if (grantResults.length != 1 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Timber.d("permission not granted");
+            }
+            else
+            {
+                Timber.d("permission granted");
+                openMediaSelector(SignUp.this);
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void openMediaSelector(Activity context){
+        Intent camIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//("android.media.action.IMAGE_CAPTURE");
+        Intent gallIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        List<ResolveInfo> info=new ArrayList<ResolveInfo>();
+        List<Intent> yourIntentsList = new ArrayList<Intent>();
+        PackageManager packageManager = context.getPackageManager();
+        List<ResolveInfo> listCam = packageManager.queryIntentActivities(camIntent, 0);
+        for (ResolveInfo res : listCam) {
+            final Intent finalIntent = new Intent(camIntent);
+            finalIntent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            yourIntentsList.add(finalIntent);
+            info.add(res);
+        }
+        List<ResolveInfo> listGall = packageManager.queryIntentActivities(gallIntent, 0);
+        for (ResolveInfo res : listGall) {
+            final Intent finalIntent = new Intent(gallIntent);
+            finalIntent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            yourIntentsList.add(finalIntent);
+            info.add(res);
+        }
+        openDialog(context,yourIntentsList,info);
+    }
+
+    private static void openDialog(final Activity context, final List<Intent> intents,
+                                   List<ResolveInfo> activitiesInfo) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context, R.style.AlertDialogTheme);
+        dialog.setTitle("Select image source");
+        final AlertDialog.Builder builder = dialog.setAdapter(buildAdapter(context, activitiesInfo),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = intents.get(id);
+                        context.startActivityForResult(intent, 1);
+                    }
+                });
+
+        dialog.setNeutralButton("Cancel",
+                new android.content.DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        dialog.show();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    mImageViewProfile.setImageURI(selectedImage);
+                }
+             break;
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    mImageViewProfile.setImageURI(selectedImage);
+                    Picasso.with(this)
+                            .load(selectedImage).into(mImageViewProfile);
+                    boolean isCamera = (imageReturnedIntent == null || imageReturnedIntent.getData() == null);
+                    if(isCamera)
+                    {
+                        Bundle extras = imageReturnedIntent.getExtras();
+                        assert extras != null;
+                        Bitmap imageBitmap = (Bitmap) extras.get("data");
+                        Picasso.with(this)
+                                .load(MediaStore.Images.Media.insertImage(getApplicationContext().getContentResolver(),
+                                        imageBitmap, "Title", null)).into(mImageViewProfile);
+                    }
+                }
+                 break;
+        }
+    }
+
+    private static ArrayAdapter<ResolveInfo> buildAdapter(final Context context, final List<ResolveInfo> activitiesInfo) {
+        return new ArrayAdapter<ResolveInfo>(context, R.layout.image_picker,R.id.textview_imagepicker_title,activitiesInfo){
+            @NonNull
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                ResolveInfo res=activitiesInfo.get(position);
+                ImageView image= view.findViewById(R.id.imageview_imagepicker_icon);
+                image.setImageDrawable(res.loadIcon(context.getPackageManager()));
+                TextView textview= view.findViewById(R.id.textview_imagepicker_title);
+                textview.setText(res.loadLabel(context.getPackageManager()).toString());
+                return view;
+            }
+        };
     }
 }
