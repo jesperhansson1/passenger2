035dfe2dab0d9b277c704d493a7f53ca3d48b8fe
Jesper Hansson <jesper.hansson@cybercom.com>
2018-04-10 11:33:25 +0200

    Send and receive reject-passenger notifications

--- app/src/main/java/com/cybercom/passenger/MainViewModel.java
+++ app/src/main/java/com/cybercom/passenger/MainViewModel.java
@@ -136,8 +136,9 @@ public class MainViewModel extends AndroidViewModel {
         mPassengerRepository.sendNotification(acceptPassenger);
     }
 
-    public void sendRejectPassengerNotificaiton() {
-        // TODO: implement
+    public void sendRejectPassengerNotification(Drive drive, DriveRequest driveRequest) {
+        Notification rejectPassenger = new Notification(Notification.REJECT_PASSENGER, driveRequest, drive);
+        mPassengerRepository.sendNotification(rejectPassenger);
 
     }
 
