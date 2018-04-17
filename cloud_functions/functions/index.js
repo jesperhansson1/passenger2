07627e9a79768c77d2d8bee8c87913794af22e84
Andreas Vettefors <andreas.vettefors@cybercom.com>
2018-04-17 11:36:59 +0200

    Change cloud function to the new notification object

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -4,7 +4,7 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.pushNotifications = functions.database.ref('notifications/{notificationId}')
-  .onCreate((snapshot, context) => {
+  .onWrite((change, context) => {
 
     const DRIVE_REQUEST = 0;
     const PASSENGER_ACCEPT = 1;
@@ -12,9 +12,9 @@ exports.pushNotifications = functions.database.ref('notifications/{notificationI
 
     var notificationId = context.params.notificationId;
 
-    var notificationData = snapshot.val();
-    var drive = notificationData.drive;
-    var driveRequest = notificationData.driveRequest;
+    var notificationData = change.after.val();
+    var driveId = notificationData.driveId;
+    var driveRequestId = notificationData.driveRequestId;
 
     var payload = {};
     var notificationSound = "default";
@@ -25,106 +25,80 @@ exports.pushNotifications = functions.database.ref('notifications/{notificationI
     var acceptedDriveRequestTitle = "Accepted drive request";
     var acceptedDriveRequestBody = "has accepted your drive request";
 
-    const driverPromise = admin.database().ref(`/users/${drive.driverId}`).once('value');
-    const passengerPromise = admin.database().ref(`/users/${driveRequest.passenger}`).once('value');
+    const drivePromise = admin.database().ref(`/drives/${driveId}`).once('value');
+    const driveRequestPromise = admin.database().ref(`/driveRequests/${driveRequestId}`).once('value');
 
-    return Promise.all([driverPromise, passengerPromise]).then(results => {
-      var driver = results[0].val();
-      var passenger = results[1].val();
+    return Promise.all([drivePromise, driveRequestPromise]).then(results => {
+      var drive = results[0].val();
+      var driveRequest = results[1].val();
 
-      if (notificationData.type === DRIVE_REQUEST) {
-        recieverTokenId = driver.notificationTokenId;
+      const driverPromise = admin.database().ref(`/users/${drive.driverId}`).once('value');
+      const passengerPromise = admin.database().ref(`/users/${driveRequest.passenger}`).once('value');
 
-        payload = {
-          notification: {
-            title: driveRequestTitle,
-            body: `${passenger.fullName} ${driveRequestBody}`,
-            sound: notificationSound
-          },
-          data: {
-            type: DRIVE_REQUEST.toString(),
-            driveDriverId: drive.driverId,
-            driveAvailableSeats: drive.availableSeats.toString(),
-            driveStartLocationLatitude: drive.startLocation.latitude.toString(),
-            driveStartLocationLongitude: drive.startLocation.longitude.toString(),
-            driveEndLocationLatitude: drive.endLocation.latitude.toString(),
-            driveEndLocationLongitude: drive.endLocation.longitude.toString(),
-            driveTime: drive.time.toString(),
+      return Promise.all([driverPromise, passengerPromise]).then(results => {
+        var driver = results[0].val();
+        var passenger = results[1].val();
 
-            driveRequestPassenger: driveRequest.passenger,
-            driveRequestExtraPassengers: driveRequest.extraPassengers.toString(),
-            driveRequestStartLocationLatitude: driveRequest.startLocation.latitude.toString(),
-            driveRequestStartLocationLongitude: driveRequest.startLocation.longitude.toString(),
-            driveRequestEndLocationLatitude: driveRequest.endLocation.latitude.toString(),
-            driveRequestEndLocationLongitude: driveRequest.endLocation.longitude.toString(),
-            driveRequestTime: driveRequest.time.toString()
-          }
-        };
+        if (notificationData.type === DRIVE_REQUEST) {
+          recieverTokenId = driver.notificationTokenId;
 
-        console.log(payload);
-      }
+          payload = {
+            notification: {
+              title: driveRequestTitle,
+              body: `${passenger.fullName} ${driveRequestBody}`,
+              sound: notificationSound
+            },
+            data: {
+              type: DRIVE_REQUEST.toString(),
+              driveId: driveId.toString(),
+              driveRequest: driveRequestId.toString(),
 
-      if (notificationData.type === PASSENGER_ACCEPT) {
-        recieverTokenId = passenger.notificationTokenId;
+            }
+          };
 
-        payload = {
-          notification: {
-            title: acceptedDriveRequestTitle,
-            body: `${driver.fullName} ${acceptedDriveRequestBody}`,
-            sound: notificationSound
-          },
-          data: {
-            type: PASSENGER_ACCEPT.toString(),
-            driveDriverId: drive.driverId,
-            driveAvailableSeats: drive.availableSeats.toString(),
-            driveStartLocationLatitude: drive.startLocation.latitude.toString(),
-            driveStartLocationLongitude: drive.startLocation.longitude.toString(),
-            driveEndLocationLatitude: drive.endLocation.latitude.toString(),
-            driveEndLocationLongitude: drive.endLocation.longitude.toString(),
-            driveTime: drive.time.toString(),
+          console.log(payload);
+        }
 
-            driveRequestPassenger: driveRequest.passenger,
-            driveRequestExtraPassengers: driveRequest.extraPassengers.toString(),
-            driveRequestStartLocationLatitude: driveRequest.startLocation.latitude.toString(),
-            driveRequestStartLocationLongitude: driveRequest.startLocation.longitude.toString(),
-            driveRequestEndLocationLatitude: driveRequest.endLocation.latitude.toString(),
-            driveRequestEndLocationLongitude: driveRequest.endLocation.longitude.toString(),
-            driveRequestTime: driveRequest.time.toString()
-          }
-        };
-      }
+        if (notificationData.type === PASSENGER_ACCEPT) {
+          recieverTokenId = passenger.notificationTokenId;
 
-      if (notificationData.type === PASSENGER_REJECT) {
-        recieverTokenId = passenger.notificationTokenId;
+          payload = {
+            notification: {
+              title: acceptedDriveRequestTitle,
+              body: `${driver.fullName} ${acceptedDriveRequestBody}`,
+              sound: notificationSound
+            },
+            data: {
+              type: PASSENGER_ACCEPT.toString(),
+              driveId: driveId.toString(),
+              driveRequest: driveRequestId.toString()
+            }
+          };
 
-        payload = {
-          data: {
-            type: PASSENGER_REJECT.toString(),
-            driveDriverId: drive.driverId,
-            driveAvailableSeats: drive.availableSeats.toString(),
-            driveStartLocationLatitude: drive.startLocation.latitude.toString(),
-            driveStartLocationLongitude: drive.startLocation.longitude.toString(),
-            driveEndLocationLatitude: drive.endLocation.latitude.toString(),
-            driveEndLocationLongitude: drive.endLocation.longitude.toString(),
-            driveTime: drive.time.toString(),
+          console.log(payload);
+        }
 
-            driveRequestPassenger: driveRequest.passenger,
-            driveRequestExtraPassengers: driveRequest.extraPassengers.toString(),
-            driveRequestStartLocationLatitude: driveRequest.startLocation.latitude.toString(),
-            driveRequestStartLocationLongitude: driveRequest.startLocation.longitude.toString(),
-            driveRequestEndLocationLatitude: driveRequest.endLocation.latitude.toString(),
-            driveRequestEndLocationLongitude: driveRequest.endLocation.longitude.toString(),
-            driveRequestTime: driveRequest.time.toString()
-          }
-        };
-      }
-      return admin.messaging().sendToDevice(recieverTokenId, payload)
-        .then((response) => {
-          const error = response.results[0].error;
-          if(error){
-            console.error("Couldn't send notification", error);
-          }
-          return admin.database().ref(`/notifications/${notificationId}`).remove();
-        });
+        if (notificationData.type === PASSENGER_REJECT) {
+          recieverTokenId = passenger.notificationTokenId;
+
+          payload = {
+            data: {
+              type: PASSENGER_REJECT.toString(),
+              driveId: driveId.toString(),
+              driveRequest: driveRequestId.toString()
+            }
+          };
+
+          console.log(payload);
+        }
+        return admin.messaging().sendToDevice(recieverTokenId, payload)
+          .then((response) => {
+            const error = response.results[0].error;
+            if (error) {
+              console.error("Couldn't send notification", error);
+            }
+           return "hej"; //admin.database().ref(`/notifications/${notificationId}`).remove();
+          });
+      });
     });
   });
