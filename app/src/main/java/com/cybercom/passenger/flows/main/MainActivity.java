7b96ee0ec21d90093e74df0443ec2ba49d887dcb
Andreas Vettefors <andreas.vettefors@cybercom.com>
2018-05-22 14:15:32 +0200

    Change design on tracking notification

--- app/src/main/java/com/cybercom/passenger/flows/main/MainActivity.java
+++ app/src/main/java/com/cybercom/passenger/flows/main/MainActivity.java
@@ -16,6 +16,7 @@ import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
