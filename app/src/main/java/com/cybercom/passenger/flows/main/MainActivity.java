b1aa4174adc6531f92c7777bd21af7144d2564b8
Jesper Hansson <jesper.hansson@cybercom.com>
2018-06-28 10:13:52 +0200

    [Bugfix] Add missing import to fix broken build

--- app/src/main/java/com/cybercom/passenger/flows/main/MainActivity.java
+++ app/src/main/java/com/cybercom/passenger/flows/main/MainActivity.java
@@ -96,6 +96,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.squareup.picasso.Picasso;
 
 import java.net.MalformedURLException;
 import java.net.URL;
